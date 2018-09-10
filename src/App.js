5f1e5643a97072ced75e8df9cd8bd31846b3e2f3
Gareth Jones <garethjones@cybertron.local>
2018-09-10 20:58:17 +0100

    added bootstrap, font-awesome, and a nav bar with app name title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 // import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hello</h1>
+        <Navbar />
       </div>
     );
   }
