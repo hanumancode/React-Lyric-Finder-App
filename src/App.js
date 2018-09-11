ee6580f003f3f7b54e1c89f85e6142a9e29183e8
Gareth Jones <garethjones@cybertron.local>
2018-09-11 20:06:42 +0100

    added a navbar and imported react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-// import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <Router>
+        <React.Fragment>  
+          <Navbar />
+        </React.Fragment>
+      </Router>
     );
   }
 }
